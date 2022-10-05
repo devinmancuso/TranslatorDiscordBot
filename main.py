318c230e4075ab854f8949118e9d2ba77c57347d
Devin Mancuso <devin@radelaide.net>
2022-10-05 00:15:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from datetime import datetime, timedelta
 
 # Set intents - see: https://discordpy.readthedocs.io/en/stable/intents.html
-intents = disord.Intents.default()
+intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(
